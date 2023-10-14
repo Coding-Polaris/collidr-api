7f7adbc092d01397195b7985796f6e38748632ca
CP <seth.swenson@protonmail.com>
2023-10-14 09:43:34 -0400

    Add RatingQueue subscription to rating create/update

--- config/initializers/listeners.rb
+++ config/initializers/listeners.rb
@@ -3,4 +3,7 @@ Rails.application.reloader.to_prepare do
   Wisper.subscribe(ActivityListener, on: :created_post)
   Wisper.subscribe(ActivityListener, on: :created_user)
   Wisper.subscribe(ActivityListener, on: :user_above_four_stars)
+
+  Wisper.subscribe(RatingQueue, on: :created_rating)
+  Wisper.subscribe(RatingQueue, on: :updated_rating)
 end
